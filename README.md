d9870ab995264aba4023aede341a97c1900caad9
Koma Human <hammer.99.ivan@gmail.com>
2022-12-05 14:01:08 +0300

    init: ruby bot

---
+++ README.md
@@ -0,0 +1,4 @@
+<div align='center'>
+<h1>Telegram bots on 12 Programming languages</h1>
+
+</div>
---
+++ Ruby/telegramBot.rb
@@ -0,0 +1,22 @@
+require 'telegram_bot'
+
+bot = TelegramBot.new(token: 'FAKE TOKEN')
+bot.get_updates(fail_silently: true) do |message|
+  puts "@#{message.from.username}: #{message.text}"
+  command = message.get_command_for(bot)
+
+  message.reply do |reply|
+    case command
+    when /greet/i
+      reply.text = "Hello, #{message.from.first_name}!"
+    when /telegram/i
+        reply.text = "TELEGRAM!!!!!!!"
+    when /прив/i
+        reply.text = "прив)"
+    else 
+      reply.text = "#{message.from.first_name}, have no idea what #{command.inspect} means."
+    end
+    puts "sending #{reply.text.inspect} to @#{message.from.username}"
+    reply.send_with(bot)
+  end
+end
